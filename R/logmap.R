653057843a25d768be93861f69c2510c153e9854
Diego Diez <diego10ruiz@gmail.com>
2018-04-13 21:34:22 +0900

    Fix documentation.

--- R/logmap.R
+++ R/logmap.R
@@ -2,7 +2,7 @@
 #'
 #' @param r0 initial value for the r parameter.
 #' @param r1 final value for the r parameter.
-#' @param step increment value for the r parameter.
+#' @param length.out increment value for the r parameter.
 #' @param maxit maximum number of iterations returned per r value.
 #' @export
 logmap <- function(r0 = 0, r1 = 4, length.out = 1000, maxit = 100) {
