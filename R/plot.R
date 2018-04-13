7da3cd8d0b51b0d811e62d515dc1de416d07df7f
Diego Diez <diego10ruiz@gmail.com>
2018-04-14 00:17:45 +0900

    Fix default parameters for plot.logmap() method.

--- R/plot.R
+++ R/plot.R
@@ -3,6 +3,7 @@
 #' Plot a logistic map object.
 #'
 #' @param x logmap object.
+#' @param ... ignored.
 #' @param size point size.
 #' @param color point color.
 #' @param xlim limits for x-axis.
@@ -10,7 +11,7 @@
 #' @return NULL
 #' @export
 #'
-plot.logmap <- function(x, size = 1, color = "black", xlim = c(0, 4)) {
+plot.logmap <- function(x, ..., size = 1, color = "black", xlim = c(0, 4)) {
   x <- as.data.frame(unclass(x))
   ggplot(x, aes_string(x = "r", y = "y")) +
     geom_point(shape = ".", size = size, color = color) +
